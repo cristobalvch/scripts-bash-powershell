d10c9f7833e5ebf5d224c32a92610f5287f64239
cristobalvch <cristobalvch29@gmail.com>
2024-01-25 15:33:41 +0100

    added python script

---
+++ crtsh.py
@@ -0,0 +1,20 @@
+import json
+import sys
+import urllib.request
+
+url_name = input("Enter the domain/subdomain (example.com): ")
+
+filename = input("File Name to Write: ")
+sys.stdout = open(filename, 'w')
+
+with urllib.request.urlopen('https://crt.sh/?q=%.'+url_name+'&output=json') as url:
+	data = json.loads(url.read().decode())
+	i = 0
+	while i == i:
+		try:
+			print(data[i]['name_value'],)
+			i=i+1
+		except (IndexError) as err:
+			pass
+			break
+sys.stdout.close()
